db79e5fd7a7f306d43d59f12095cd9485bdbcaba
mshahanwaz <mohdshahanwaz55@gmail.com>
2022-02-06 18:10:02 +0530

    Added KMP2 algorithm

---
+++ kmp_algorithm2.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> prefix_function(string s) {
+	int n = (int)s.length();
+	vector<int> pi(n);
+	for(int i = 1; i < n; i++) {
+		int j = pi[i - 1];
+		while(j > 0 && s[j] != s[i]) {
+			j = pi[j - 1];
+		}
+		if(s[i] == s[j]) ++j;
+		pi[i] = j;
+	}
+	return pi;
+}
+
+void solve() {
+	
+}
+
+int main() {
+	cin.tie(0) -> sync_with_stdio(0);
+	int tc = 1; cin >> tc;
+	while(tc--) solve();
+	return 0;
+}
+
